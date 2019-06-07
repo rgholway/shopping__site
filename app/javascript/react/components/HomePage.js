b4fe1d5bb4c763ab7c8b1183718b8b04146af017
Richard Holway <rgholway@syr.edu>
2019-06-07 16:29:07 -0400

    Added menu to phone

--- app/javascript/react/components/HomePage.js
+++ app/javascript/react/components/HomePage.js
@@ -5,9 +5,12 @@ class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      menuActive: ""
     }
     this.fetchItems = this.fetchItems.bind(this)
+    this.handleMenuClick = this.handleMenuClick.bind(this)
+    this.handleCloseMenuClick = this.handleCloseMenuClick.bind(this)
   }
 
   fetchItems() {
@@ -32,7 +35,16 @@ class HomePage extends Component {
     this.fetchItems()
   }
 
+  handleMenuClick() {
+    this.setState({menuActive: "active"})
+  }
+
+  handleCloseMenuClick() {
+    this.setState({menuActive: ""})
+  }
+
   render() {
+    console.log(this.state.menuActive);
     let itemsArray = this.state.items.map(item => {
       return(
         <ItemTile
@@ -50,9 +62,16 @@ class HomePage extends Component {
       )
     })
     return (
+        <div>
           <div className="shopping">
             {itemsArray}
           </div>
+          <div className="title"></div>
+          <div className={`phone__menu--${this.state.menuActive}`}>
+            <h1 className={`phone__menu__lines--${this.state.menuActive}`} onClick={this.handleMenuClick}>|||</h1>
+            <h1 className={`active__menu__lines--${this.state.menuActive}`} onClick={this.handleCloseMenuClick}>|||</h1>
+          </div>
+        </div>
     )
   }
 }
